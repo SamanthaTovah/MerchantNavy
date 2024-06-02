eb76b4010414ee6ab64101aa903e0f4ffa846e35
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2024-06-02 18:06:30 +0200

    spring initializr

---
+++ src/main/java/io/github/samanthatovah/merchantnavy/AuroraMerchantNavyApplication.java
@@ -0,0 +1,13 @@
+package io.github.samanthatovah.merchantnavy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuroraMerchantNavyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuroraMerchantNavyApplication.class, args);
+	}
+
+}
